7bb01ef4869b54d08729709ac07468b157f09250
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-02-24 11:06:45 +0300

    Update I18nClientBridge.tsx

--- web/i18n/I18nClientBridge.tsx
+++ web/i18n/I18nClientBridge.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import { useGlobal } from "@/context/GlobalContext";
 import { I18nProvider } from "./I18nProvider";
 
@@ -7,3 +8,4 @@ export function I18nClientBridge({ children }: { children: React.ReactNode }) {
   const { uiSettings } = useGlobal();
   return <I18nProvider language={uiSettings.language}>{children}</I18nProvider>;
 }
+
