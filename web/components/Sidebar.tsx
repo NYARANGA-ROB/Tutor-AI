326e51b837f9e33c7041b7710bb119b9f0c5ff66
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-02-25 07:12:02 +0300

    Update Sidebar.tsx

--- web/components/Sidebar.tsx
+++ web/components/Sidebar.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useState, useRef, useEffect, useMemo } from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -512,3 +511,4 @@ export default function Sidebar() {
     </div>
   );
 }
+
