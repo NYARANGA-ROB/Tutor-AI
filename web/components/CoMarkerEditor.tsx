b0ec6180f5d90b923df2a87532c4dff738ca4563
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-02-25 07:06:35 +0300

    Update CoMarkerEditor.tsx

--- web/components/CoMarkerEditor.tsx
+++ web/components/CoMarkerEditor.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useState, useRef, useEffect, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import {
@@ -2127,3 +2126,4 @@ export default function CoWriterEditor({
     </div>
   );
 }
+
